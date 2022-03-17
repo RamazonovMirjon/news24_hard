b99cffdddb33dfee23accd62e34cf00b5e7cdcf2
Mirjon Ramazonov <mirxan2564@gmail.com>
2022-03-17 05:06:02 +0500

    5 sahifa tayyor

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -1,12 +1,6 @@
-// To parse this JSON data, do
-//
-//     final newsModel = newsModelFromJson(jsonString);
-
 import 'dart:convert';
 
 NewsModel newsModelFromJson(String str) => NewsModel.fromJson(json.decode(str));
-
-String newsModelToJson(NewsModel data) => json.encode(data.toJson());
 
 class NewsModel {
   NewsModel({
@@ -25,12 +19,6 @@ class NewsModel {
         articles: List<Article>.from(
             json["articles"].map((x) => Article.fromJson(x))),
       );
-
-  Map<String, dynamic> toJson() => {
-        "status": status,
-        "totalResults": totalResults,
-        "articles": List<dynamic>.from(articles!.map((x) => x.toJson())),
-      };
 }
 
 class Article {
@@ -56,7 +44,7 @@ class Article {
 
   factory Article.fromJson(Map<String, dynamic> json) => Article(
         source: Source.fromJson(json["source"]),
-        author: json["author"] == null ? null : json["author"],
+        author: json["author"],
         title: json["title"],
         description: json["description"],
         url: json["url"],
@@ -64,17 +52,6 @@ class Article {
         publishedAt: DateTime.parse(json["publishedAt"]),
         content: json["content"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "source": source!.toJson(),
-        "author": author == null ? null : author,
-        "title": title,
-        "description": description,
-        "url": url,
-        "urlToImage": urlToImage,
-        "publishedAt": publishedAt!.toIso8601String(),
-        "content": content,
-      };
 }
 
 class Source {
@@ -87,12 +64,7 @@ class Source {
   String? name;
 
   factory Source.fromJson(Map<String, dynamic> json) => Source(
-        id: json["id"] == null ? null : json["id"],
+        id: json["id"],
         name: json["name"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "id": id == null ? null : id,
-        "name": name,
-      };
 }
--- lib/screens/interest_page.dart
+++ lib/screens/interest_page.dart
@@ -8,8 +8,92 @@ class InterestsPage extends StatefulWidget {
 }
 
 class _InterestsPageState extends State<InterestsPage> {
+  List<String> text = [
+    "World News",
+    "Politics",
+    "Technology",
+    "Science",
+    "Business",
+    "Entertainment",
+    "Food"
+  ];
+  List<bool> bolist = List.generate(7, (i) => true);
+  bool all = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Interests',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                "All",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              trailing: Switch.adaptive(
+                activeColor: Colors.black,
+                value: all,
+                onChanged: (v) {
+                  setState(() {
+                    all = !all;
+                    for (int i = 0; i < bolist.length; i++) {
+                      bolist[i] = all;
+                    }
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: ListView.separated(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: bolist.length,
+                separatorBuilder: (_, __) => ListTile(
+                  title: Text(text[__]),
+                  trailing: Switch.adaptive(
+                    activeColor: Colors.black,
+                    value: bolist[__],
+                    onChanged: (v) {
+                      setState(() {
+                        bolist[__] = !bolist[__];
+                      });
+                    },
+                  ),
+                ),
+                itemBuilder: (_, __) => const Divider(),
+              ),
+            ),
+            Expanded(child: Center(child: _buttomMethod))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget get _buttomMethod {
+    return SizedBox(
+      height: 40,
+      width: 150,
+      child: ElevatedButton(
+          child: const Text("Start"),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+          onPressed: () {}),
+    );
   }
 }
